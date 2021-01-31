f19ea70b3970e8baf21ebd64c4b8946811695025
Your Name <mehedihasan019898@gmail.com>
2021-02-01 01:06:42 +0600

    some file attach

---
+++ defoult.js
@@ -0,0 +1,6 @@
+function add (num1, num2 = 50){
+    return num1 + num2;
+}
+
+const result = add(458, 3);
+console.log(result);
---
+++ template.js
@@ -0,0 +1,11 @@
+const firstName = "Justin";
+const lastName = "TimberLake";
+const fullName = firstName + ' ' + lastName + 'justin is a good boy';
+const fullName2 = `${firstName} ${47+87}  a good boy and also he is a good armor man`
+
+const multiline = "i love you\n" + "i miss you\n" + "i need you";
+const multilineEs = `I Love You 
+I Need You 
+I Miss You 
+No. I Dont Need You Right Now`
+console.log(multilineEs);
